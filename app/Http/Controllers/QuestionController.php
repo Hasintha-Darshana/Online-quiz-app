96136fb0fc57b6dcc55c42347d6c6008e98e25f3
hasintha <hasinthadarshana3@gmail.com>
2024-09-26 10:29:16 +0530

    Create question and answers store and installed laravel debugbar package

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Answer;
+use App\Models\Question;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
 use Illuminate\Foundation\Application;
@@ -9,13 +11,56 @@ use Illuminate\Http\Request;
 
 class QuestionController extends Controller
 {
+    public array $validateRules = ['required', 'string'];
     public function createQuestion(): View|Factory|Application
     {
         return view('admin.questions.create-question');
     }
 
-    public function addQuestion(Request $request): View|Factory|Application
+    public function addQuestion(Request $request)
     {
-        dd($request->all());
+        $this->validateQuestionRequest($request);
+
+
+        $question = Question::create([
+            'question' =>   $request['question'],
+            'correct_answer' =>   $request['correct'],
+        ]);
+
+        if(!$request){
+            return redirect()->route('dashboard');
+        }
+        $answers =[
+            'answer1' =>   $request['answer1'],
+            'answer2' =>   $request['answer2'],
+            'answer3' =>   $request['answer3'],
+            'answer4' =>   $request['answer4'],
+
+        ];
+
+        foreach($answers as $answer){
+            Answer::create([
+                'quiz_id'=>$question->id,
+                'answer'=>$answer,
+            ]);
+        }
+        return redirect()->route('dashboard');
+    }
+
+    /**
+     * @param Request $request
+     * @return void
+     */
+    public function validateQuestionRequest(Request $request): void
+    {
+        $request->validate([
+            'question' => $this->validateRules,
+            'correct' => $this->validateRules,
+            'answer1' => $this->validateRules,
+            'answer2' => $this->validateRules,
+            'answer3' => $this->validateRules,
+            'answer4' => $this->validateRules,
+
+        ]);
     }
 }
--- app/Models/Answer.php
+++ app/Models/Answer.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Answer extends Model
 {
@@ -14,4 +16,9 @@ class Answer extends Model
         'quiz_id',
         'answer'
     ];
+
+    public function questions(): BelongsTo
+    {
+        return $this->belongsTo(Question::class);
+    }
 }
